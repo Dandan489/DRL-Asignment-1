6a7d9a2e1914d8cc1a19ceab5be22732abd9ae86
DanDan <dan0922.dan0922@gmail.com>
2025-03-15 23:11:59 +0800

    0.15 Tune randomness

--- student_agent.py
+++ student_agent.py
@@ -139,19 +139,13 @@ def get_action(obs):
     ref_obs = refine_obs(obs, stage, substage, past_obs, last_action, prev_pickup)
 
     action = 0
-    # if stage == 1:
-    #     action = 4
-    # elif stage == 3:
-    #     action = 5
-    # else:
     if(ref_obs in Q_table):
         action = np.argmax(Q_table[ref_obs])
-        if (np.random.rand() < 0.1 and action != 4 and action != 5): 
+        if (np.random.rand() < 0.15 and action != 4 and action != 5): 
             action = random_pick(obs, last_action)
     else:
         action = random_pick(obs, last_action)
     
     past_obs = obs
     last_action = action
-    # print(Q_table[ref_obs])
     return action
